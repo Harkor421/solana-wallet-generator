3d00e7bfd83e2bcd25ec83d6ce48cdbc8b254870
Samir Gonzalez <sekesie2@gmail.com>
2024-08-14 16:02:38 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,36 @@
+## what it does
+
+![](screenshot.png)
+
+this tool comes in handy if you want to start a solana project and want a customized solana address.
+it basically generates addresses and outputs you the credentials for accounts that have a matching name.
+Usually when displaying Solana addresses, most services cut everything between the first 4 and the last 4 digits.
+This tool gives you the opportunity to get a cool address for your project!
+Please do not use it extensively because you basically spam the Solana network with it.
+
+## install
+
+
+git clone this repository, then switch to the new folder and execute
+
+```
+npm install
+```
+
+
+## how to use
+
+edit the file index.js and change the parameters on the top of the file for your needs
+
+then
+```
+npm start
+```
+
+## faster alternatives
+
+you should check out this repo for GPU based address gathering
+https://github.com/mcf-rocks/solanity
+
+
+___
---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const bs58 = require('bs58');
+const solanaWeb3 = require('@solana/web3.js');
+const Solana = new solanaWeb3.Connection("https://ssc-dao.genesysgo.net/");
+
+// IF YOU ARE A NOOB JUST EDIT THIS PART
+const stopAfter = 100; // setting this to 0 deactivates the limit
+const outputFile = "./output/solana_keys.txt"; // output file
+
+// FUNCTION PART
+
+// Create output file if it does not exist
+if (!fs.existsSync(outputFile)) {
+    fs.writeFileSync(outputFile, ""); // Create an empty file if it does not exist
+}
+
+// Write key to file
+const writeSolKey = async (keyPair) => {
+    const publicKey = keyPair.publicKey.toString();
+    const secretKey = bs58.encode(keyPair.secretKey);
+    const content = `Public Key: ${publicKey}\nPrivate Key: ${secretKey}\n\n`; // Added \n for separation
+
+    fs.appendFileSync(outputFile, content);
+    console.log(`Keys written to ${outputFile}`);
+}
+
+const solKeyGen = async () => {
+    console.log('Solana Keypair Generator started.');
+    let count = 0;
+    console.log('Please be patient while the computer generates the keypairs.');
+
+    while ((count < stopAfter) || stopAfter === 0) {
+        // Generate new keypair
+        const keyPair = solanaWeb3.Keypair.generate();
+
+        count++;
+        console.log(`[${count}] Generated - Public Key: ${keyPair.publicKey.toString()}`);
+        await writeSolKey(keyPair);
+    }
+};
+
+solKeyGen();
